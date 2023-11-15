02c7a51e7a5c912841762b3e88fcf735f54f8de5
wildflower0424 <106394504+wildflower0424@users.noreply.github.com>
2023-11-15 16:53:45 -0700

    Create Cloudflared-Install.sh

---
+++ Cloudflared-Install.sh
@@ -0,0 +1,5 @@
+curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb && 
+
+sudo dpkg -i cloudflared.deb && 
+
+sudo cloudflared service install eyJhIjoiMTc3ZDAwMGFmOTlmNzU4ZGY5OWU3ZWIzMDA1MzAxYTUiLCJ0IjoiOWE2M2M0MWQtZjI1Yi00N2QxLWJkYjYtOTI2MTJjYmY2OTBjIiwicyI6Ik5qUmtaV1ZqWW1NdE9HSmhPUzAwTldKa0xXSTBPR1l0T0RZek5USXhPR00xTmpCayJ9
